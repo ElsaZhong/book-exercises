502e625d7fb298d34da41d49a249a7a7b308367e
Elsa Zhong <elsazh926@gmail.com>
2020-02-28 22:43:13 -0800

    Add exercises

--- chapter-19-exercises/exercise-4/app_ui.R
+++ chapter-19-exercises/exercise-4/app_ui.R
@@ -7,27 +7,39 @@ library("shiny")
 # Define a variable `intro_panel` for your first page. It should be a
 # `tabPanel()` with a title "Introduction" to represent the first tab.
 # This layout will contain the following elements:
+intro_panel <- tabPanel(
+  "Introduction",
   
   # A `titlePanel()` with the text "Income Inequality"
+  titlePanel("Income Inequality"),
   
-
   # A paragraph `p()` describing with the text: "The below diagram was created
   # by the New York Times to illustrate the increasing level of inequality in
   # the US."
+  p("The below diagram was created by the New York Times to illustrate the
+    increasing level of inequality in the US."),
   
-
   # An image `img()` to display. This content should have a `src` attribute of
   # "inequality.png" (which will refer to the file in the `www/` folder).
+  img(src = "inequality.png"),
   
-
   # A paragraph containing a hyperlink `a()` to the source of the article at
   # https://www.nytimes.com/interactive/2017/08/07/opinion/leonhardt-income-inequality.html
-
+  p(a(href = "https://www.nytimes.com/interactive/2017/08/07/opinion/leonhardt-income-inequality.html", "(source)")),
+  
 
   # A paragraph containing a 1-2 sentence description of what the diagram
   # shows. Your paragraph should include both `strong()` and emphasized `em()`
   # text.
-  
+  p(
+    "The graph shows how the richest 1% of people have seen their incomes jump",
+    strong("6%"),
+    "since 1980",
+    "while the bottom 60% have barely seen any increase",
+    em("(their income has stagnated).")
+  ),
+
+)
 
 # Next, we'll define a few UI elements to render in our second panel
 
@@ -36,7 +48,10 @@ library("shiny")
 
   # A `sliderInput()` for the 'percentile' value, labeled "Income Percentile".
   # This slider should let the user pick a range between 0 and 100
-
+sliderInput(
+  inputId = "percentile",
+  label = "Income Percentile", min = 0, max = 100, value = c(0, 100)
+)
 
 # Define a variable `main_content` as a `mainPanel()` UI element
 # containing the following information:
